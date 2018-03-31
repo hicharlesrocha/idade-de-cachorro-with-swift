2207158b8862e90ec092663271eff0eac0a008ba
Hicharles Rocha <hicharlesolvr@gmail.com>
2018-03-31 14:08:15 -0300

    Criando o primeiro app do curso

---
+++ idade de cachorro/idade de cachorro/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  idade de cachorro
+//
+//  Created by Hicharles Rocha on 31/03/2018.
+//  Copyright © 2018 Hicharles Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "a idade do cachorro é: " + String(idade)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
